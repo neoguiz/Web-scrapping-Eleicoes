9715ca438ccf10519ef858fa98555ead8fc8a0d6
Guilherme D. Barbosa <gui.diasbarbosa2003@gmail.com>
2022-05-19 11:52:45 -0300

    Version save before updating for both candidates
    Packages installing is disabled for now, it takes too much time

--- Tweets.py
+++ Tweets.py
@@ -4,18 +4,30 @@ import sys
 def install(package):
    subprocess.check_call([sys.executable, "-m", "pip", "install", package, "--user"])
 
-install("python-twitter-v2")
-install("snscrape")
-install("pandas")
+# install("python-twitter-v2")
+# install("snscrape")
+# install("pandas")
+# install("pandasgui")
 
 # INICIO DO CODIGO
 
 import snscrape.modules.twitter as dados
-import pandas as pandas
+import pandas as pd
 import datetime
+from pandasgui import show
 
+lista_tt_bolsonaro = []
+data_final = datetime.date.today()
+data_inicial = '2022-1-1'
 
-
+for i, tweet in enumerate(dados.TwitterSearchScraper(f'{"Bolsonaro"} + since:{data_inicial} until:{data_final}').get_items()):
+   if i>500:
+      break
+   lista_tt_bolsonaro.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+   
+df_bolsonaro = pd.DataFrame(lista_tt_bolsonaro, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+gui = show(df_bolsonaro)
+print(data_final)
 
 
 
