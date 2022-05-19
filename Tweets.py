4ec65fd7bb0d2f2ad30777c51dda37e1f22e5054
Guilherme D. Barbosa <gui.diasbarbosa2003@gmail.com>
2022-05-19 20:12:36 -0300

    Added main function to search tweet
    Thinking about transforming this into a function only file and call it at main

--- Tweets.py
+++ Tweets.py
@@ -16,18 +16,30 @@ import pandas as pd
 import datetime
 from pandasgui import show
 
-lista_tt_bolsonaro = []
+def lista_tweets(candidato):
+   
+   lista_tt_candidato = []
+   
+   for i, tweet in enumerate(dados.TwitterSearchScraper(f'{candidato} + since:{data_inicial} until:{data_final}').get_items()):
+      if i>500:
+         break
+      lista_tt_candidato.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+      
+   return lista_tt_candidato
+
 data_final = datetime.date.today()
 data_inicial = '2022-1-1'
 
-for i, tweet in enumerate(dados.TwitterSearchScraper(f'{"Bolsonaro"} + since:{data_inicial} until:{data_final}').get_items()):
-   if i>500:
-      break
-   lista_tt_bolsonaro.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
-   
+lista_tt_bolsonaro = lista_tweets("Bolsonaro")   
+lista_tt_lula = lista_tweets("Lula")
+
 df_bolsonaro = pd.DataFrame(lista_tt_bolsonaro, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+df_lula = pd.DataFrame(lista_tt_lula, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+
 gui = show(df_bolsonaro)
+gui = show(df_lula)
 print(data_final)
+
 
 
 
